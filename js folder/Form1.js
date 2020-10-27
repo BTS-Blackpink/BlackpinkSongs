ff699fdce6a9e7618b7195de1d3f68522c9ce5e0
BTS-Blackpink <67686589+BTS-Blackpink@users.noreply.github.com>
2020-10-27 14:56:37 +0530

    Add files via upload

--- js folder/Form1.js
+++ js folder/Form1.js
@@ -6,54 +6,54 @@ class Form1 {
       this.header2 = createElement('h2');
       this.header3 = createElement('h2');
       //audios
-      this.a = createA('https://www.youtube.com/watch?v=rrBO9fy6A3s', "How you like that", "_self");
-      this.b = createA('https://www.youtube.com/watch?v=ja_Rqf4AFR4', "Ice Cream feat.Selena Gomez", "_self");
-      this.c = createA('https://www.youtube.com/watch?v=D8RZbI1jfQ0', "Lovesick Girls", "_self");
-      this.d = createA('https://www.youtube.com/watch?v=gXBdvSj9F2I', "Bet you wanna feat.Cardi B", "_self");
-      this.e = createA('https://www.youtube.com/watch?v=u7rKGj13pAs', "Crazy over you", "_self");
-      this.f = createA('https://www.youtube.com/watch?v=wlzGXcTzdzU', "Love to hate me", "_self");
-      this.g = createA('https://www.youtube.com/watch?v=4Kk_iaaHd_Y', "You never know", "_self");
-      this.h = createA('https://www.youtube.com/watch?v=F8c8f2nK82w', "Pretty Savage", "_self");
-      this.i = createA('https://www.youtube.com/watch?v=He322O1JWgU', "Really", "_self");
-      this.j = createA('https://www.youtube.com/watch?v=FHn4P9Fyj0M', "Don't know what to do", "_self");
-      this.k = createA('https://www.youtube.com/watch?v=IoiaAA4vNaI', "Stay", "_self");
-      this.l = createA('https://www.youtube.com/watch?v=ei0k_1Uro-U', "Playing with fire", "_self");
-      this.m = createA('https://www.youtube.com/watch?v=-gibBYpzZbI', "Forever young", "_self");
-      this.n = createA('https://www.youtube.com/watch?v=IPzouyj7FLA', "Ddu du ddu du", "_self");
-      this.o = createA('https://www.youtube.com/watch?v=x0-q6-PBpPU', "Whistle", "_self");
-      this.p = createA('https://www.youtube.com/watch?v=TXeYZKqzkac', "As if it's your last", "_self");
-      this.q = createA('https://www.youtube.com/watch?v=18nDrsoii5M', "Boombayah", "_self");
-      this.r = createA('https://www.youtube.com/watch?v=Il_KNBTmLpQ', "Kill this love", "_self");
-      this.s = createA('https://www.youtube.com/watch?v=fnPn6At3v28', "Sour Candy - Lady Gaga feat. Blackpink", "_self");
-      this.t = createA('https://www.youtube.com/watch?v=AX3Bsiq-13k', "Kiss and make up - Dua Lipa feat. Blackpink", "_self");
-      this.ae = createA('https://www.youtube.com/watch?v=-gZC9hC1PDQ', "Kick it", "_self");
+      this.a = createA('http://www.youtube.com/watch?v=rrBO9fy6A3s', "How you like that", "_self");
+      this.b = createA('http://www.youtube.com/watch?v=ja_Rqf4AFR4', "Ice Cream feat.Selena Gomez", "_self");
+      this.c = createA('http://www.youtube.com/watch?v=D8RZbI1jfQ0', "Lovesick Girls", "_self");
+      this.d = createA('http://www.youtube.com/watch?v=gXBdvSj9F2I', "Bet you wanna feat.Cardi B", "_self");
+      this.e = createA('http://www.youtube.com/watch?v=u7rKGj13pAs', "Crazy over you", "_self");
+      this.f = createA('http://www.youtube.com/watch?v=wlzGXcTzdzU', "Love to hate me", "_self");
+      this.g = createA('http://www.youtube.com/watch?v=4Kk_iaaHd_Y', "You never know", "_self");
+      this.h = createA('http://www.youtube.com/watch?v=F8c8f2nK82w', "Pretty Savage", "_self");
+      this.i = createA('http://www.youtube.com/watch?v=He322O1JWgU', "Really", "_self");
+      this.j = createA('http://www.youtube.com/watch?v=FHn4P9Fyj0M', "Don't know what to do", "_self");
+      this.k = createA('http://www.youtube.com/watch?v=IoiaAA4vNaI', "Stay", "_self");
+      this.l = createA('http://www.youtube.com/watch?v=ei0k_1Uro-U', "Playing with fire", "_self");
+      this.m = createA('http://www.youtube.com/watch?v=-gibBYpzZbI', "Forever young", "_self");
+      this.n = createA('http://www.youtube.com/watch?v=IPzouyj7FLA', "Ddu du ddu du", "_self");
+      this.o = createA('http://www.youtube.com/watch?v=x0-q6-PBpPU', "Whistle", "_self");
+      this.p = createA('http://www.youtube.com/watch?v=TXeYZKqzkac', "As if it's your last", "_self");
+      this.q = createA('http://www.youtube.com/watch?v=18nDrsoii5M', "Boombayah", "_self");
+      this.r = createA('http://www.youtube.com/watch?v=Il_KNBTmLpQ', "Kill this love", "_self");
+      this.s = createA('http://www.youtube.com/watch?v=fnPn6At3v28', "Sour Candy - Lady Gaga feat. Blackpink", "_self");
+      this.t = createA('http://www.youtube.com/watch?v=AX3Bsiq-13k', "Kiss and make up - Dua Lipa feat. Blackpink", "_self");
+      this.ae = createA('http://www.youtube.com/watch?v=-gZC9hC1PDQ', "Kick it", "_self");
 
       //MVs
-      this.u = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Solo - Jennie", "_self");
-      this.v = createA('https://www.youtube.com/watch?v=ioNng23DkIM', "How you like that", "_self");
-      this.w = createA('https://www.youtube.com/watch?v=vRXZj0DzXIA', "Ice cream", "_self");
+      this.u = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Solo - Jennie", "_self");
+      this.v = createA('http://www.youtube.com/watch?v=ioNng23DkIM', "How you like that", "_self");
+      this.w = createA('http://www.youtube.com/watch?v=vRXZj0DzXIA', "Ice cream", "_self");
+      this.x = createA('http://www.youtube.com/watch?v=dyRsYk0LyA8', "Lovesick Girls", "_self");
 
-      this.x = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Lovesick Girls", "_self");
-      this.y = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Stay", "_self");
-      this.z = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Playing with fire", "_self");
-      this.aa = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Ddu du ddu du", "_self");
-      this.ab = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Whistle", "_self");
-      this.ac = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "As if it's your last", "_self");
-      this.ad = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Boombayah", "_self");
+      this.y = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Stay", "_self");
+      this.z = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Playing with fire", "_self");
+      this.aa = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Ddu du ddu du", "_self");
+      this.ab = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Whistle", "_self");
+      this.ac = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "As if it's your last", "_self");
+      this.ad = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Boombayah", "_self");
 
       //Dance performance/Dance practice
-      this.af = createA('https://www.youtube.com/watch?v=32si5cfrCNc', "How you like that", "_self");
-      this.ag = createA('https://www.youtube.com/watch?v=27h9SJcIdM0', "Ice cream", "_self");
-      this.ah = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Lovesick girls", "_self");
-      this.ai = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Don't know what to do", "_self");
-      this.aj = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Playing with fire", "_self");
-      this.ak = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Forever young", "_self");
-      this.al = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Ddu du ddu du", "_self");
-      this.am = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Whistle", "_self");
-      this.an = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "As if it's your last", "_self");
-      this.ao = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Boombayah", "_self");
-      this.ap = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Kill this love", "_self");
-      this.aq = createA('https://www.youtube.com/watch?v=b73BI9eUkjM', "Solo", "_self");
+      this.af = createA('http://www.youtube.com/watch?v=32si5cfrCNc', "How you like that", "_self");
+      this.ag = createA('http://www.youtube.com/watch?v=27h9SJcIdM0', "Ice cream", "_self");
+      this.ah = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Lovesick girls", "_self");
+      this.ai = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Don't know what to do", "_self");
+      this.aj = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Playing with fire", "_self");
+      this.ak = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Forever young", "_self");
+      this.al = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Ddu du ddu du", "_self");
+      this.am = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Whistle", "_self");
+      this.an = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "As if it's your last", "_self");
+      this.ao = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Boombayah", "_self");
+      this.ap = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Kill this love", "_self");
+      this.aq = createA('http://www.youtube.com/watch?v=b73BI9eUkjM', "Solo", "_self");
     }
    
     display(){
